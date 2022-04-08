c664eb6cf9073e9fdd9dd53d5e5ae3ef32803a51
SisiliaIndah18 <79432738+SisiliaIndah18@users.noreply.github.com>
2022-04-08 14:18:18 +0700

    first commit

---
+++ project.pae.a/src/main/java/crud/ktp/project/pae/a/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package crud.ktp.project.pae.a.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
